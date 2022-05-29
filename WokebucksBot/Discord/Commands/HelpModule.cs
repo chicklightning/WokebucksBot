2f833bf0e1a11f619fd6e88c94ea9349a7c50460
Gabrielle Sweet <sweet@xbox.com>
2022-05-29 04:05:07 -0700

    add help module

---
+++ WokebucksBot/Discord/Commands/HelpModule.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace Swamp.WokebucksBot.Discord.Commands
+{
+    public  class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        private const string CommandName = "CommandName";
+        private const string UserIdKey = "UserId";
+
+        private readonly ILogger<BalanceModule> _logger;
+        private readonly CommandService _commandService;
+
+        public HelpModule(ILogger<BalanceModule> logger, CommandService commandService)
+        {
+            _logger = logger;
+            _commandService = commandService;
+        }
+
+        [Command("help")]
+        public async Task Help()
+        {
+            _logger.LogInformation($"<{{{CommandName}}}> command invoked by user <{{{UserIdKey}}}>.", "help", Context.User.GetFullUsername());
+
+            List<CommandInfo> commands = _commandService.Commands.ToList();
+
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.WithTitle("Wokebucks Bot Commands");
+            embedBuilder.WithColor(Color.Gold);
+            embedBuilder.WithFooter($"{Context.User.GetFullUsername()}'s Help Request resolved by Wokebucks");
+            foreach (CommandInfo command in commands)
+            {
+                // Get the command Summary attribute information
+                string embedFieldText = command.Summary ?? "No description available\n";
+
+                embedBuilder.AddField(command.Name, embedFieldText);
+            }
+
+            await ReplyAsync("", false, embedBuilder.Build());
+        }
+    }
+}
